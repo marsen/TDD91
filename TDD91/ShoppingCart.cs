f9f36ef70d6d8f47830d717b0fbaf662a9c56602
marklin <marklin@91app.com>
2017-10-03 15:50:11 +0800

    TEST PROJ

---
+++ TDD91/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD91
+{
+    public class ShoppingCart
+    {
+        public decimal CheckOut(ShoppingCartContext context)
+        {
+            return 0m;
+        }
+    }
+}
---
+++ TDD91Tests/ShoppingCartTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using TDD91;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD91.Tests
+{
+    public class ShoppingCartTests
+    {
+        [Fact()]
+        public void CheckOutTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
